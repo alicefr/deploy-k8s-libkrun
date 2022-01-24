9548c1ce83774517edaf51615484b7ca0e227424
Alice Frosi <afrosi@redhat.com>
2022-01-24 11:38:26 +0100

    Add modprobe br_filter and ipv2 forwarding

--- k8s-node-preparation/start-k8s.sh
+++ k8s-node-preparation/start-k8s.sh
@@ -4,6 +4,8 @@ set -xe
 swapoff -a
 kubeadm init --config /var/lib/installation/kubeadm.yaml
 mkdir -p /root/.kube/
+modprobe br_netfilter
+echo '1' > /proc/sys/net/ipv4/ip_forward
 cp /etc/kubernetes/admin.conf /root/.kube/config
 kubectl apply -f /var/lib/installation/calico.yaml
 kubectl taint nodes --all node-role.kubernetes.io/master-
