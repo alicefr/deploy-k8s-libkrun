14b68509414cd1aabd0f09036a1be719639fba64
Alice Frosi <afrosi@redhat.com>
2022-01-25 16:18:56 +0100

    Add comment on which tests we are executing

--- critest/start-critest.sh
+++ critest/start-critest.sh
@@ -7,9 +7,13 @@ sudo podman run --name $CONT_NAME -v /lib/modules:/lib/modules:ro \
 	critest
 # Let crio starting
 sleep 5
+# Skip tests that
+#  - pull the images because we are only interested in the runtime
+#  - exec command not supported
 sudo podman exec -ti $CONT_NAME critest --annotation run.oci.handler="krun" \
 	-ginkgo.skip="Image Manager" \
-	-ginkgo.skip="runtime should support execSync"
+	-ginkgo.skip="runtime should support execSync" \
+	-ginkgo.skip="runtime should support SupplementalGroups"
 
 sudo podman stop $CONT_NAME
 sudo podman rm $CONT_NAME
