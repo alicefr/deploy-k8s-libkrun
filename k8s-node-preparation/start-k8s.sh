18150f68a832a483b5e5d16fd275dbeec53afe08
Alice Frosi <afrosi@redhat.com>
2022-01-25 16:12:12 +0100

    Load br_netfilter kernel module if missig

--- k8s-node-preparation/start-k8s.sh
+++ k8s-node-preparation/start-k8s.sh
@@ -4,7 +4,7 @@ set -xe
 swapoff -a
 kubeadm init --config /var/lib/installation/kubeadm.yaml
 mkdir -p /root/.kube/
-modprobe br_netfilter
+lsmod | grep br_netfilter || modprobe br_netfilter
 echo '1' > /proc/sys/net/ipv4/ip_forward
 cp /etc/kubernetes/admin.conf /root/.kube/config
 kubectl apply -f /var/lib/installation/calico.yaml
